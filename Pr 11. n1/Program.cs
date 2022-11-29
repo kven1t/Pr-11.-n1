2617b1da9d0a166792f365dc20504f7e44cbb643
hp <hp@HP>
2022-11-29 20:19:32 +0300

    Добавьте файлы проекта.

---
+++ Pr 11. n1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace Pr_11._n1
+{
+    internal class Program
+    {
+        static int fact(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * fact(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            double s = 0, x = 0.7;
+            int n = 1;
+            do
+            {
+                x = (Math.Pow(x , 2 * n)) /fact(2 * n);
+                s += x;
+                n++;
+            }
+            while (Math.Abs(x) > 0.0001);
+            Console.WriteLine("Сумма: " + s);
+        }
+    }
+}
